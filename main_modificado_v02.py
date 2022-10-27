761f11b3bd35affcbe4297d5f3107778f1409385
Leo Hoet <leohoet98@gmail.com>
2022-10-27 20:49:31 -0300

    Refactored and added smoke tests

--- main_modificado_v02.py
+++ main_modificado_v02.py
@@ -1,34 +1,58 @@
 #!/usr/bin/python3.7
 """ExactParetoFrontFinder main file."""
 
-import os
-import re
-import errno
-import time
-import json
 import argparse
-import numpy as np
+import dataclasses
+import errno
+import json
+import os
+import time
+from dataclasses import dataclass
+from typing import List, Dict
+
+from pyomo.core.base.PyomoModel import Model
+from pyomo.environ import SolverFactory, summation
+from pyomo.environ import value
+from pyomo.opt import OptSolver
+
 import nrp
-from pyomo.environ import SolverFactory,  summation
 
 START = 'start'
 DAT_LD = 'data_load'
 MD_RUN = 'model_run'
 PLOT = 'plot'
-
-"""
-12/08/2020
-    Version 02 del main de pareto front finder
-        Modificado:
-        - Se utiliza pyomo para la creacion de modelos
-        - Se pueden utilizar todos los solvers que soporta pyomo
-        - El archivo de ahora es un .dat especificado en: https://pyomo.readthedocs.io/en/stable/working_abstractmodels/data/datfiles.html?highlight=.dat
-        - Borrado funciones load_data, format_data, load_model
-"""
+SOLVER_THREADS = 16
 
 
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
 
-def create_model(data_path:str):
+
+@dataclass
+class RunResult:
+    iteration: int
+    profit: float
+    cost: float
+    x: str
+    y: str
+    time: float
+
+    @classmethod
+    def from_args(self, iteration: int, profit: float, cost: float, x: str, y: str, time: float) -> 'RunResult':
+        return RunResult(
+            iteration=iteration,
+            profit=profit,
+            cost=cost,
+            x=x,
+            y=y,
+            time=time
+        )
+
+
+def create_model(data_path: str) -> Model:
     """ Creates a concrete model of the nrp problem
         :param data_path: Where the data file is located
         :return: nrp concrete model
@@ -36,8 +60,7 @@ def create_model(data_path:str):
     return nrp.abstract_model().create_instance(data_path)
 
 
-
-def fill_model(instance,e_constraint_val: float = None):
+def fill_model(instance, e_constraint_val: float = None):
     """Fills an abstract  model with e_constraint if e_constraint_val != None
     :param instance: A concrete model
     :param e_constraint_val: Value to profit the cost value, defaults to None
@@ -48,23 +71,23 @@ def fill_model(instance,e_constraint_val: float = None):
     if e_constraint_val:
         try:
             e_constraint_val = int(e_constraint_val)
-            instance.l[1] = summation(instance.cost,instance.x) <= e_constraint_val
+            instance.l[1] = summation(instance.cost, instance.x) <= e_constraint_val
         except KeyError:
-            instance.l.add(summation(instance.cost,instance.x) <= e_constraint_val)
+            instance.l.add(summation(instance.cost, instance.x) <= e_constraint_val)
     return instance
 
 
-def run(concrete_model, solver) -> dict:
-    """ Runs the concrete model with the specified solver
-    :param concrete_model: A Model already created by pyomo library
-    :param solver: A constructed solver to solve the model
-    :return: Dictionary representation of the profits and costs
-    :rtype: dict
+def run(concrete_model: Model, solver: OptSolver, current_iteration: int) -> RunResult:
     """
+    """
+    solve_kwargs = {}
     if solver.name != 'glpk':
-        res = solver.solve(concrete_model,warmstart=True)
-    else:
-        res = solver.solve(concrete_model)
+        solve_kwargs['warmstart'] = True
+
+    t0 = time.perf_counter()
+    res = solver.solve(concrete_model, **solve_kwargs)
+    t1 = time.perf_counter()
+
     x = ""
     y = ""
     for i in concrete_model.x.values():
@@ -72,28 +95,15 @@ def run(concrete_model, solver) -> dict:
     for i in concrete_model.y.values():
         y += str(int(i.value))
 
-    profits = sum(concrete_model.profit[c] * concrete_model.y[c].value for c in concrete_model.customers)
-    costs = sum(concrete_model.cost[r] * concrete_model.x[r].value for r in concrete_model.requierements)
-    return {'profit': profits, 'cost': costs, 'x': x, 'y': y}
+    x_as_hex = f'{int(x):x}'
+    y_as_hex = f'{int(y):x}'
 
+    profits = value(concrete_model.OBJ)
+    costs = value(concrete_model.cost_constraint)
 
-def plot(x_values: [int], y_values: [int]):
-    """Plots the gotten values
-    :param x_values: X-axis values
-    :type x_values: int[]
-    :param y_values: Y-axis values
-    :type y_values: int[]
-    """
-
-    bokeh_plot.output_file("./output/graphs.html")
-    tools = "hover,crosshair,pan,wheel_zoom,box_zoom,reset,tap,save,box_select,poly_select"
-    plot_obj = bokeh_plot.figure(title="Gráficos", x_axis_label='Costos', y_axis_label='Ganancias',
-                                 tools=tools)
-    x_color = np.random.random(size=len(x_values)) * 100
-    y_color = np.random.random(size=len(y_values)) * 100
-    colors = ["#%02x%02x%02x" % (int(r), int(g), 150) for r, g in zip(50+2*x_color, 30+2*y_color)]
-    plot_obj.circle(x_values, y_values, size=10, fill_alpha=0.5, fill_color=colors)
-    bokeh_plot.save(plot_obj)
+    return RunResult.from_args(
+        current_iteration, profits, costs, x_as_hex, y_as_hex, t1 - t0
+    )
 
 
 def write_times(time_struct: dict):
@@ -117,7 +127,7 @@ def write_times(time_struct: dict):
                    (round(total_t, 7), round(total_t * 100 / total_t)))
 
 
-def model_run(instance,arguments: dict):
+def model_run(instance, arguments: dict) -> List[RunResult]:
     """Runs the model and retrieves all the gotten results
     :param instance: A concrete pyomo model
     :param arguments: Command-line arguments
@@ -126,33 +136,54 @@ def model_run(instance,arguments: dict):
     :rtype: list
     """
 
-    result = []
-    solver = SolverFactory(arguments['solver']) # Create solver to use
-    if solver.name in ['cbc','cplex']:          # If solver support multiple threads
-        solver.options['threads'] = 4           # Assing n threads  to solver. Be cautious with overheading
+    def cost_l(x):
+        return x > float(arguments['cost']) if arguments['cost'] else lambda x: True
+
+    def profit_l(x):
+        return x > float(arguments['profit']
+                         ) if arguments['profit'] else lambda x: True
+
+    results = []
     epsilon = float(arguments['epsilon'])
-    data_path = arguments['data_path']
+    inform_each = int(arguments['informEach'])
 
-    def cost_l(x): return x > float(arguments['cost']) if arguments['cost'] else lambda x: True
-    def profit_l(x): return x > float(arguments['profit']
-                                      ) if arguments['profit'] else lambda x: True
-    informEach = int(arguments['informEach'])
-    def over_zero(x): return x['cost'] > 0 and x['profit'] > 0
+    solver = SolverFactory(arguments['solver'])  # Create solver to use
+    solver_supports_threads = solver.name in ['cbc', 'cplex']
+    if solver_supports_threads:
+        solver.options['threads'] = SOLVER_THREADS
 
-    result.append({'iteration': 0, **run(fill_model(instance), solver)})
+    iteration = 0
+    model = fill_model(instance)
+    results.append(run(model, solver, iteration))
 
-    
-    iteration = 1
-    # result[iteration - 1]['cost'] = 500
-    while (iteration < int(arguments['iterations'])) and over_zero(result[iteration - 1]) and \
-            cost_l(result[iteration - 1]['cost']) and profit_l(result[iteration - 1]['profit']):
-        model = fill_model(instance,result[iteration - 1]['cost'] - epsilon)
-        result.append({'iteration': iteration, **run(model, solver)})
-        if iteration % informEach == 0:
+    iteration += 1
+    max_iterations = int(arguments['iterations'])
+    while (iteration < max_iterations) \
+            and results[-1].profit > 0 \
+            and results[-1].cost > 0 \
+            and cost_l(results[-1].cost) \
+            and profit_l(results[-1].profit):
+
+        model = fill_model(instance, results[-1].cost - epsilon)
+        results.append(run(model, solver, iteration))
+
+        if iteration % inform_each == 0:
             print('Iteration {}: Cost = {}, Profit = {}'.format(
-                iteration, result[iteration - 1]['cost'], result[iteration - 1]['profit']))
+                iteration, results[-1].cost, results[-1].profit)
+            )
         iteration += 1
-    return result
+    return results
+
+
+def write_results(results: List[RunResult]):
+    try:
+        os.makedirs('./output')
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+    with open('./output/result.json', 'w+') as file:
+        json.dump(results, file, cls=EnhancedJSONEncoder)
 
 
 def main(arguments: dict):
@@ -163,23 +194,16 @@ def main(arguments: dict):
     time_struct = dict()
     time_struct[START] = time.time()
     c_model = create_model(arguments['data_path'])
-    time_struct[DAT_LD] = time.time()
-    result = model_run(c_model,arguments)
-    time_struct[MD_RUN] = time.time()
-    plot(list(map(lambda x: x['cost'], result)), list(map(lambda x: x['profit'], result)))
-    time_struct[PLOT] = time.time()
-    # creates output folder if it doesn't exists
-    try:
-        os.makedirs('./output')
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
-    # Writes the results to the final file
-    with open('./output/result.json', 'w+') as file:
-        json.dump(result, file)
 
-    write_times(time_struct)
-    # glpsol --cuts --fpump --mipgap 0.001 --model problem.mod --data problem.dat
+    time_struct[DAT_LD] = time.time()
+    results = model_run(c_model, arguments)
+    time_struct[MD_RUN] = time.time()
+
+    time_struct[PLOT] = time.time()
+
+    write_results(results)
+
+    # write_times(time_struct)
 
 
 def get_parser():
@@ -203,10 +227,10 @@ def get_parser():
                         help='Defines the limit for the profit objetive')
     parser.add_argument('--epsilon', dest='epsilon', default='0.001',
                         help='Defines the epsilon value in order to limit/bound certain variable' +
-                        '(default: 0.001)')
+                             '(default: 0.001)')
     parser.add_argument('--informEach', dest='informEach', default='10',
                         help='Defines how often the algorithm reports partial results.' +
-                        '(default: 10 iterations)')
+                             '(default: 10 iterations)')
     return parser.parse_args()
 
 
---
+++ test_main.py
@@ -0,0 +1,25 @@
+import dataclasses
+import json
+from unittest import TestCase
+
+from pyomo.opt import SolverFactory
+
+from main_modificado_v02 import create_model, run, RunResult
+
+
+class MainUnitTest(TestCase):
+
+    def setUp(self) -> None:
+        data_path = 'nrp_100c_140r.dat'
+        solver_name = 'cbc'
+        self.concrete_model = create_model(data_path)
+        self.solver = SolverFactory(solver_name)
+
+    def test_run_smoke(self):
+        run(self.concrete_model, self.solver)
+
+    def test_run_result_to_json_smoke(self):
+        result = RunResult(
+            1, 1, 1, '1', '1', 1
+        )
+        json.dumps(dataclasses.asdict(result))
